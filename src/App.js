e002a20a24e6b9b20261cd6652d3333e6a17f8d9
aliensmart <kaoua17@gmail.com>
2019-11-08 12:25:05 -0500

    cursor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.png';
+import profile from './images/Round-Profile-Pic.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App main">
+      <div className="sideBar">
+        <div>
+          <img src={logo} alt="LOGO" className="logo"/>
+        </div>
+        
+          Hello I am the sideBare
+      </div>
+      <div className="content">
+        <div className="content_top">
+          <div className="search">
+              <input type="text" placeholder="search" className="searchInput"/>
+              <i className="fas fa-search"></i>
+          </div>
+          <img src={profile} alt="profile" className="profile"/>
+          
+        </div>
+        <div className="content_bottom">
+            I am the main content
+        </div>
+        
+      </div>
     </div>
   );
 }
