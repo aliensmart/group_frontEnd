d0f750e490f1c6d21aad869b1028243ebb05ae9f
aliensmart <kaoua17@gmail.com>
2019-11-08 17:08:00 -0500

    user Loggin created Provider Login created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import logo from './images/logo.png';
 import profile from './images/Round-Profile-Pic.png'
 import './App.css';
 
+import UserHome from './components/LoggedView/User/Home'
+
 function App() {
+  const token = sessionStorage.getItem("token")
+  let content = null
+  if(!token){
+    content = (
+      <div>
+      <form>
+        <label for="username">Username</label>
+        <input type="text" placeholder="username" id="username"/>
+        <label for="password">Password</label>
+        <input type="text" placeholder="password" id="password"/>
+        <input type="button" value="login"/>
+      </form>
+      <a href="#">Don't have an account?</a>
+      </div>
+    )
+  }
+
   return (
     <div className="App main">
       <div className="sideBar">
@@ -11,7 +30,20 @@ function App() {
           <img src={logo} alt="LOGO" className="logo"/>
         </div>
         
-          Hello I am the sideBare
+          <ul className="nav">
+            <li className="nav_item">
+              <a href="#" className="nav_item_content" >Link1</a>
+            </li>
+            <li className="nav_item">
+              <a href="#" className="nav_item_content" >Link2</a>
+            </li>
+            <li className="nav_item">
+              <a href="#" className="nav_item_content" >Link3</a>
+            </li>
+            <li className="nav_item">
+              <a href="#" className="nav_item_content" >Link4</a>
+            </li>
+          </ul>
       </div>
       <div className="content">
         <div className="content_top">
@@ -23,7 +55,12 @@ function App() {
           
         </div>
         <div className="content_bottom">
-            I am the main content
+            <UserHome/>
+            <div className="content_bottom_sidebare">
+              <button className="btn btn_main btn_black">Create new File</button>
+              <button className="btn btn_main btn_white">Upload file</button>
+              <button className="btn btn_main btn_white">Upload directory</button>
+            </div>
         </div>
         
       </div>
---
+++ src/components/NotLoggedView/Provider/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ProviderLogin = ()=>{
+    return(
+        <div>
+      <form>
+        <label for="username">Username</label>
+        <input type="text" placeholder="username" id="username"/>
+        <label for="password">Password</label>
+        <input type="text" placeholder="password" id="password"/>
+        <input type="button" value="login"/>
+      </form>
+      <a href="#">Don't have an account?</a>
+      </div>
+    )
+}
+
+export default ProviderLogin
